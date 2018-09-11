34a07c71c24121b2478dae9d00c19bee1f1fed78
shen <343206898@qq.com>
2018-09-11 16:15:15 +0800

    实现无缝轮播

--- main.js
+++ main.js
@@ -1,31 +1,24 @@
 $('.images > img:nth-child(1)').addClass('current')
 $('.images > img:nth-child(2)').addClass('enter')
 $('.images > img:nth-child(3)').addClass('enter')
-setTimeout(() => {
-    $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
-    .one('transitionend',(e)=>{
-        $(e.currentTarget).removeClass('leave').addClass('enter')
-    })
-    $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
+
+let n = 1;
+setInterval(() => {
+
+    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+        .one('transitionend', (e) => {
+            $(e.currentTarget).removeClass('leave').addClass('enter')
+        })
+    $(`.images > img:nth-child(${x(n + 1)})`).removeClass('enter').addClass('current')
+    n += 1
 }, 1000)
-setTimeout(() => {
-    $('.images > img:nth-child(2)').removeClass('current').addClass('leave')
-    .one('transitionend',(e)=>{
-        $(e.currentTarget).removeClass('leave').addClass('enter')
-    })
-    $('.images > img:nth-child(3)').removeClass('enter').addClass('current')
-}, 2000)
-setTimeout(() => {
-    $('.images > img:nth-child(3)').removeClass('current').addClass('leave')
-    .one('transitionend',(e)=>{
-        $(e.currentTarget).removeClass('leave').addClass('enter')
-    })
-    $('.images > img:nth-child(1)').removeClass('enter').addClass('current')
-}, 3000)
-setTimeout(() => {
-    $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
-    .one('transitionend',(e)=>{
-        $(e.currentTarget).removeClass('leave').addClass('enter')
-    })
-    $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
-}, 4000)
+
+function x(n){
+    if(n>3){
+        n = n%3
+        if (n === 0){
+            n = 3
+        }
+    } // n = 1 2 3
+    return n
+}
