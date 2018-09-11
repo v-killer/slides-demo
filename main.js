9d446da4f0fed876a7c9d5bd745a2b472702a4c4
shen <343206898@qq.com>
2018-09-11 16:16:40 +0800

    代码优化

--- main.js
+++ main.js
@@ -1,24 +1,50 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-
-let n = 1;
+let n
+初始化()
 setInterval(() => {
 
-    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+    makeLeave(getImage(n))
         .one('transitionend', (e) => {
-            $(e.currentTarget).removeClass('leave').addClass('enter')
+            makeEnter($(e.currentTarget))
         })
-    $(`.images > img:nth-child(${x(n + 1)})`).removeClass('enter').addClass('current')
+    makeCurrent(getImage(n+1))
     n += 1
 }, 1000)
 
-function x(n){
-    if(n>3){
-        n = n%3
-        if (n === 0){
+
+
+
+
+
+
+
+// 封装函数
+function getImage(n) {
+    return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function x(n) {
+    if (n > 3) {
+        n = n % 3
+        if (n === 0) {
             n = 3
         }
     } // n = 1 2 3
     return n
+}
+
+function 初始化() {
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+        .siblings().addClass('enter')
+}
+
+function makeCurrent($node) {
+    return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node) {
+    return $node.removeClass('enter current').addClass('leave')
+     // 操作node,返回node
+}
+function makeEnter($node) {
+    return $node.removeClass('leave current').addClass('enter')
 }
