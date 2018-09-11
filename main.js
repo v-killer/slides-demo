ff0b30ee7ec8ed09450583861b72cc4694f6a691
shen <343206898@qq.com>
2018-09-11 16:13:44 +0800

    行为样式分离

--- main.js
+++ main.js
@@ -1,45 +1,31 @@
-setTimeout(function () {
-    $('.images>img:nth-child(1)').css({
-        transform: 'translateX(-100%)'
+$('.images > img:nth-child(1)').addClass('current')
+$('.images > img:nth-child(2)').addClass('enter')
+$('.images > img:nth-child(3)').addClass('enter')
+setTimeout(() => {
+    $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
+    .one('transitionend',(e)=>{
+        $(e.currentTarget).removeClass('leave').addClass('enter')
     })
-    $('.images>img:nth-child(2)').css({
-        transform: 'translateX(-100%)'
-    })
-    $('.images>img:nth-child(1)').one('transitionend',function(e){
-        $(e.currentTarget).addClass('right').css({transform: 'none'})
-    })
+    $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
 }, 1000)
-
-setTimeout(function(){
-    $('.images>img:nth-child(2)').css({
-        transform: 'translateX(-200%)'
+setTimeout(() => {
+    $('.images > img:nth-child(2)').removeClass('current').addClass('leave')
+    .one('transitionend',(e)=>{
+        $(e.currentTarget).removeClass('leave').addClass('enter')
     })
-    $('.images>img:nth-child(3)').css({
-        transform: 'translateX(-100%)'
+    $('.images > img:nth-child(3)').removeClass('enter').addClass('current')
+}, 2000)
+setTimeout(() => {
+    $('.images > img:nth-child(3)').removeClass('current').addClass('leave')
+    .one('transitionend',(e)=>{
+        $(e.currentTarget).removeClass('leave').addClass('enter')
     })
-    $('.images>img:nth-child(2)').one('transitionend',function(e){
-        $(e.currentTarget).addClass('right').css({transform: 'none'})
+    $('.images > img:nth-child(1)').removeClass('enter').addClass('current')
+}, 3000)
+setTimeout(() => {
+    $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
+    .one('transitionend',(e)=>{
+        $(e.currentTarget).removeClass('leave').addClass('enter')
     })
-},2000)
-setTimeout(function(){
-    $('.images>img:nth-child(3)').css({
-        transform: 'translateX(-200%)'
-    })
-    $('.images>img:nth-child(1)').css({
-        transform: 'translateX(-100%)'
-    })
-    $('.images>img:nth-child(3)').one('transitionend',function(e){
-        $(e.currentTarget).addClass('right').css({transform: 'none'})
-    })
-},3000)
-setTimeout(function(){
-    $('.images>img:nth-child(1)').css({
-        transform: 'translateX(-200%)'
-    })
-    $('.images>img:nth-child(2)').css({
-        transform: 'translateX(-100%)'
-    })
-    $('.images>img:nth-child(1)').one('transitionend',function(e){
-        $(e.currentTarget).addClass('right').css({transform: 'none'})
-    })
-},4000)
+    $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
+}, 4000)
